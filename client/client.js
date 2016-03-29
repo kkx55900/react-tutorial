f9510f38e6277678b7aff44213ddeaafe8f421ae
EasonWang01 <jason40115@hotmail.com>
2016-03-29 17:14:31 +0800

    first router

--- client/client.js
+++ client/client.js
@@ -1,8 +1,11 @@
 import React from 'react'
 import { render } from 'react-dom'
 import App from '../components/App'
+import { Router, Route, hashHistory } from 'react-router'
 
-render(
-  <App/>,
-  document.getElementById('app')
-)
+render(( 
+	<Router history={hashHistory}>
+     <Route path="/" component={App}/>
+    </Router> 
+  ),document.getElementById('app'))
+
